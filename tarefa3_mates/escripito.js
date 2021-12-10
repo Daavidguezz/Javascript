226fce69aa388b1494e8f9145631e7559b1518a1
Daavidguezz <92926097+Daavidguezz@users.noreply.github.com>
2021-12-10 13:34:42 +0100

    Tarefa 3

---
+++ tarefa3_mates/escripito.js
@@ -0,0 +1,30 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+var moito = 25 ;
+    var raiz = Math.sqrt(moito);
+    alert(raiz);
+
+
+var jajaja = 4.4523;
+var jejeje = 2.9843;
+var jijiji = 1.7534;
+var ultima = 3.6323;
+
+var cadrado = Math.pow(jajaja, 2);
+	alert(cadrado);
+
+
+
+alert(Math.round(Math.PI));
+
+
+var menor = Math.min(jajaja, jejeje, jijiji, ultima);
+	alert(menor);
+
+function getRandom() {};
+alert(Math.round(Math.random(0,3)));
+
