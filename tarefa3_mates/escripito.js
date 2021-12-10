3e669f681e43cc0ad57c723ff988e22e6756f06e
Daavidguezz <92926097+Daavidguezz@users.noreply.github.com>
2021-12-10 13:52:51 +0100

    Add files via upload

--- tarefa3_mates/escripito.js
+++ tarefa3_mates/escripito.js
@@ -6,7 +6,7 @@
 
 var moito = 25 ;
     var raiz = Math.sqrt(moito);
-    alert(raiz);
+    alert("A raiz de " + moito + " é " + raiz);
 
 
 var jajaja = 4.4523;
@@ -15,16 +15,16 @@ var jijiji = 1.7534;
 var ultima = 3.6323;
 
 var cadrado = Math.pow(jajaja, 2);
-	alert(cadrado);
+	alert("O cadrado de " + jajaja + " é " + cadrado);
 
 
 
-alert(Math.round(Math.PI));
+alert("O valor de pi é " + Math.round(Math.PI));
 
 
 var menor = Math.min(jajaja, jejeje, jijiji, ultima);
 	alert(menor);
 
 function getRandom() {};
-alert(Math.round(Math.random(0,3)));
+alert("Un numero aleatorio entre 0 e 3 sería por exemplo: " + Math.round(Math.random(0,3)));
 
